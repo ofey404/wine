cca6db24c7a55f33217706a0b10da42415b0b75c
Ofey Chan <ofey206@gmail.com>
2021-06-29 16:56:51 +0800

    feat: add definitions of options

--- programs/robocopy/robocopy.c
+++ programs/robocopy/robocopy.c
@@ -21,131 +21,79 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(robocopy);
 
-#define CHAR_MAX 127
-
-/* Use a non-character as a pseudo short option,
-   starting with CHAR_MAX + 1.  */
-enum
+enum opt_indicator
 {
-    MIRROR_DIRECTORY_TREE = CHAR_MAX + 1,
-    NO_JOB_HEADER,
-    NO_JOB_SUMMARY,
-    NO_FILE_NAME_LOGGED,
-    NO_DIRECTORY_NAME_LOGGED,
-    COPY_SUBDIRECTORY_INCLUDE_EMPTY,
-    NO_LOG_FILE_SIZE,
-    NO_LOG_FILE_CLASS,
-    NO_LOG_COPY_PROGRESS,
+    COPY_SUBDIRECTORIES_EXCLUDE_EMPTY,
+    COPY_SUBDIRECTORIES_INCLUDE_EMPTY,
+    PURGE_NOT_IN_SOURCE,
+    MIRROR_DIRECTORY_TREE,
+    EXCLUDE_FILES,
+    EXCLUDE_DIRECTORY,
     EXCLUDE_OLDER_FILE,
     RETRY_ON_FAILED,
     WAIT_TIME_BETWEEN_RETRIES,
-    EXCLUDE_DIRECTORY,
-    EXCLUDE_FILES
+    NO_LOG_FILE_SIZE,
+    NO_LOG_FILE_CLASS,
+    NO_LOG_COPY_PROGRESS,
+    NO_FILE_NAME_LOGGED,
+    NO_DIRECTORY_NAME_LOGGED,
+    NO_JOB_HEADER,
+    NO_JOB_SUMMARY,
 };
 
-static struct option const long_opts[] = {
-    {"mir", no_argument, NULL, MIRROR_DIRECTORY_TREE},
-    {"njh", no_argument, NULL, NO_JOB_HEADER},
-    {"njs", no_argument, NULL, NO_JOB_SUMMARY},
-    {"nfl", no_argument, NULL, NO_FILE_NAME_LOGGED},
-    {"ndl", no_argument, NULL, NO_DIRECTORY_NAME_LOGGED},
-    {"e", no_argument, NULL, COPY_SUBDIRECTORY_INCLUDE_EMPTY},
-    {"ns", no_argument, NULL, NO_LOG_FILE_SIZE},
-    {"nc", no_argument, NULL, NO_LOG_FILE_CLASS},
-    {"np", no_argument, NULL, NO_LOG_COPY_PROGRESS},
-    {"xo", no_argument, NULL, EXCLUDE_OLDER_FILE},
-    {"r", colon_seperated_argument, NULL, RETRY_ON_FAILED},
-    {"w", colon_seperated_argument, NULL, WAIT_TIME_BETWEEN_RETRIES},
-    {"xd", space_seperated_argument_list, NULL, EXCLUDE_DIRECTORY},
-    {"xf", space_seperated_argument_list, NULL, EXCLUDE_FILES}
+static gdos_option const opts[] = {
+    /* copy options */
+    {"s", NO_ARGUMENT, COPY_SUBDIRECTORIES_EXCLUDE_EMPTY},
+    {"e", NO_ARGUMENT, COPY_SUBDIRECTORIES_INCLUDE_EMPTY},
+
+    /* FIXME: What means "allows the destination directory
+              security settings to not be overwritten." in doc? */
+    {"purge", NO_ARGUMENT, PURGE_NOT_IN_SOURCE},
+    {"mir", NO_ARGUMENT, MIRROR_DIRECTORY_TREE},
+
+    /* file selection options */
+    {"xf", SPACE_SEPERATED_ARGUMENT_LIST, EXCLUDE_FILES},
+    {"xd", SPACE_SEPERATED_ARGUMENT_LIST, EXCLUDE_DIRECTORY},
+    {"xo", NO_ARGUMENT, EXCLUDE_OLDER_FILE},
+
+    /* retry options */
+    {"r", COLON_SEPERATED_ARGUMENT, RETRY_ON_FAILED},
+    {"w", COLON_SEPERATED_ARGUMENT, WAIT_TIME_BETWEEN_RETRIES},
+
+    /* logging options */
+    {"ns", NO_ARGUMENT, NO_LOG_FILE_SIZE},
+    {"nc", NO_ARGUMENT, NO_LOG_FILE_CLASS},
+    {"np", NO_ARGUMENT, NO_LOG_COPY_PROGRESS},
+    {"nfl", NO_ARGUMENT, NO_FILE_NAME_LOGGED},
+    {"ndl", NO_ARGUMENT, NO_DIRECTORY_NAME_LOGGED},
+    {"njh", NO_ARGUMENT, NO_JOB_HEADER},
+    {"njs", NO_ARGUMENT, NO_JOB_SUMMARY},
+    /* job options */
 };
 
-void output_writeconsole(const WCHAR *str, DWORD wlen)
-{
-    DWORD count, ret;
+int robocopy_init_default_options(robocopy_options *opt) {
+    return INTERNAL_SUCCESS;
+}
 
-    ret = WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), str, wlen, &count, NULL);
-    if (!ret)
+int robocopy_parse_options(robocopy_options *opt, int argc, WCHAR *argvW[]) {
+    gdos_context ctx;
+    int opt;
+
+    /* TODO: handle this properly */
+    if (getopt_dos_create_context(&ctx, argc, argvW, opts) != 0)
+        return 1;
+
+    /* skip <source> <destination> and <files> */
+    while ((opt = getopt_dos_next(&ctx)) == GDOS_NEXT_NOT_OPTION)
+        ;
+
+    /* no flag */
+    if (opt == GDOS_OPTIONS_END)
+        /* TODO */
+        return 0;
+
+    if (argc < 3)
     {
-        DWORD len;
-        char  *msgA;
-
-        /* On Windows WriteConsoleW() fails if the output is redirected. So fall
-         * back to WriteFile(), assuming the console encoding is still the right
-         * one in that case.
-         */
-        len = WideCharToMultiByte(GetConsoleOutputCP(), 0, str, wlen, NULL, 0, NULL, NULL);
-        msgA = malloc(len);
-
-        WideCharToMultiByte(GetConsoleOutputCP(), 0, str, wlen, msgA, len, NULL, NULL);
-        WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), msgA, len, &count, FALSE);
-        free(msgA);
-    }
-}
-
-static void output_formatstring(const WCHAR *fmt, __ms_va_list va_args)
-{
-    WCHAR *str;
-    DWORD len;
-
-    len = FormatMessageW(FORMAT_MESSAGE_FROM_STRING|FORMAT_MESSAGE_ALLOCATE_BUFFER,
-                         fmt, 0, 0, (WCHAR *)&str, 0, &va_args);
-    if (len == 0 && GetLastError() != ERROR_NO_WORK_DONE)
-    {
-        WINE_FIXME("Could not format string: le=%u, fmt=%s\n", GetLastError(), wine_dbgstr_w(fmt));
-        return;
-    }
-    output_writeconsole(str, len);
-    LocalFree(str);
-}
-
-void WINAPIV output_message(unsigned int id, ...)
-{
-    WCHAR *fmt = NULL;
-    int len;
-    __ms_va_list va_args;
-
-    if (!(len = LoadStringW(GetModuleHandleW(NULL), id, (WCHAR *)&fmt, 0)))
-    {
-        WINE_FIXME("LoadString failed with %d\n", GetLastError());
-        return;
-    }
-
-    len++;
-    fmt = malloc(len * sizeof(WCHAR));
-    if (!fmt) return;
-
-    LoadStringW(GetModuleHandleW(NULL), id, fmt, len);
-
-    __ms_va_start(va_args, id);
-    output_formatstring(fmt, va_args);
-    __ms_va_end(va_args);
-
-    free(fmt);
-}
-
-void WINAPIV output_string(const WCHAR *fmt, ...)
-{
-    __ms_va_list va_args;
-
-    __ms_va_start(va_args, fmt);
-    output_formatstring(fmt, va_args);
-    __ms_va_end(va_args);
-}
-
-static int first_option_index(int argc, WCHAR *argvW[])
-{
-    /* TODO: Placeholder */
-    return 4;
-}
-
-int __cdecl wmain(int argc, WCHAR *argvW[])
-{
-    int c;
-    int firstopt = first_option_index(argc, argvW);
-    // struct robocopy_options x;
-
-    if (argc < 3) {
         /* TODO: Report error */
     }
     if (argc == 3)
@@ -155,26 +103,19 @@ int __cdecl wmain(int argc, WCHAR *argvW[])
         return 1;
     }
 
-    while ((c = getopt_long(argc - firstopt, argvW + firstopt, long_opts))
-            != -1)
-    {
-        switch (c)
-        {
-            case MIRROR_DIRECTORY_TREE:
-                /* Handle /mir */
-                /* x.MIR = optarg */
-                break;
-            case RETRY_ON_FAILED:
-                /* Handle /r:[n] */
-                /* optarg */
-                break;
-            case EXCLUDE_DIRECTORY:
-                /* Handle /xd <directory>[...] */
-                /* argsnum and optargs */
-                break;
-        }
-    }
-    
+    return 0;
+}
 
-    return 1;
+int __cdecl wmain(int argc, WCHAR *argvW[])
+{
+    robocopy_options x;
+    if (robocopy_init_default_options(&x) != INTERNAL_SUCCESS) {
+        return INTERNAL_ERROR_PLACEHOLDER;
+    }
+
+    if (robocopy_parse_options(&x, argc, argvW) != INTERNAL_SUCCESS) {
+        return INTERNAL_ERROR_PLACEHOLDER;
+    }
+
+    return ROBOCOPY_EXIT_SUCCESS;
 }
--- programs/robocopy/robocopy.h
+++ programs/robocopy/robocopy.h
@@ -22,26 +22,105 @@
 #include <stdlib.h>
 #include <windows.h>
 #include "resource.h"
+#include "getopt_dos.h"
 
-#define MAX_SUBKEY_LEN   257
+/* return values */
+
+/* 0: No files were copied. No failure was encountered.
+      No files were mismatched. The files already exist in
+      the destination directory; therefore, the copy
+      operation was skipped. */
+#define ROBOCOPY_EXIT_SKIPPED 0 
+/* 1: All files were copied successfully. */
+#define ROBOCOPY_EXIT_SUCCESS 1
+/* 2: There are some additional files in the destination
+      directory that are not present in the source directory.
+      No files were copied.  */
+#define ROBOCOPY_EXIT_ADDITIONAL_FILE_NO_COPY 2
+/* 3: Some files were copied. Additional files were present.
+      No failure was encountered. */
+#define ROBOCOPY_EXIT_ADDITIONAL_FILE_SOME_COPIED 3
+/* 5: Some files were copied. Some files were mismatched. No 
+      failure was encountered. */
+#define ROBOCOPY_EXIT_MISMATCH_FILE_SOME_COPIED 5
+/* 6: Additional files and mismatched files exist. No files 
+      were copied and no failures were encountered. This means
+      that the files already exist in the destination directory. */
+#define ROBOCOPY_EXIT_ADDITIONAL_MISMATCH_FILE_NO_COPY 6
+/* 7: Files were copied, a file mismatch was present, and
+      additional files were present. */
+#define ROBOCOPY_EXIT_ADDITIONAL_MISMATCH_FILE_COPIED 7
+/* 8: Several files did not copy. */
+#define ROBOCOPY_EXIT_SEVERAL_FILES_NOT_COPY 8
+
+/* FIXME: temp return value placeholder */
+#define INTERNAL_ERROR_PLACEHOLDER -65535
+
+/* return value indicator for internal functions. for clearance. */
+#define INTERNAL_SUCCESS 0
+#define INTERNAL_FAILURE -1
+
+typedef enum {
+    not_copy_subdir,
+    copy_subdir_exclude_empty,
+    copy_subdir_include_empty,
+} copy_subdirectory_type;
+
+typedef struct {
+    const WCHAR *source;
+    const WCHAR *destination;
+    const WCHAR **files;
+
+    /* copy options */
+    /* /s, /e, /mir */
+    copy_subdirectory_type copy_subdir;
+    /* /purge */
+    BOOL purge_not_in_source;
+
+    /* file selection options */
+    /* /xf <filename>[...] */
+    const WCHAR **exclude_file_patterns;
+    int num_file_patterns;
+    /* /xd <directory>[...]*/
+    const WCHAR **exclude_directory_patterns;
+    int num_directory_patterns;
+    /* /xo */
+    BOOL exclude_older_file;
+
+    /* retry options */
+    /* /r:<n> */
+    int num_retries_on_failed_copies;
+    /* /w:<n> */
+    int wait_second_between_retries;
+
+    /* logging options */
+    /* /ns */
+    BOOL not_log_file_size;
+    /* /nc */
+    BOOL not_log_file_class;
+    /* /np */
+    BOOL not_display_progress;
+    /* /nfl */
+    BOOL not_log_file_names;
+    /* /ndl */
+    BOOL not_log_directory_names;
+    /* /njh */
+    BOOL no_job_header;
+    /* /njs */
+    BOOL not_job_summary;
+
+    /* job options */
+} robocopy_options;
+
+/* robocopy.c */
+
+int robocopy_init_default_options(robocopy_options *opt);
+int robocopy_parse_options(robocopy_options *opt, int argc, WCHAR *argvW[]);
+
+/* output.c */
 
 void output_writeconsole(const WCHAR *str, DWORD wlen);
 void WINAPIV output_message(unsigned int id, ...);
 void WINAPIV output_string(const WCHAR *fmt, ...);
-
-/* getopt.c */
-struct option
-{
-    const char *name;
-    int has_arg;
-    int *flag;
-    int val;
-};
-
-int getopt_long(int argc, WCHAR *argvW[], const struct option opts[]);
-
-#define no_argument		0
-#define colon_seperated_argument	1
-#define space_seperated_argument_list	2
 
 #endif /* __ROBOCOPY_H__ */
